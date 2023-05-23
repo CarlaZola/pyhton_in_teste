9dc348c0e8941bba58ba00f308e163cd795e59d5
carla <carla.zollaa@gmail.com>
2023-05-23 18:36:01 -0300

    feat: estrutura de pastas e imports da cobra

---
+++ main.py
@@ -0,0 +1,12 @@
+from operacoes_matematicas.soma import somar
+from operacoes_matematicas.subtracao import subtrair
+from operacoes_matematicas.multiplicacao import multiplicar
+
+resultado_da_soma = somar(1, 2)
+resultado_da_sub = subtrair(1, 2)
+
+resultado_da_mult = multiplicar(4, 5)
+
+print(resultado_da_soma)
+print(resultado_da_sub)
+print(resultado_da_mult)
---
+++ operacoes_matematicas/multiplicacao.py
@@ -0,0 +1,8 @@
+from .soma import somar
+
+
+def multiplicar(a, b):
+    acumulador = 0
+    for i in range(b):
+        acumulador = somar(acumulador, a)
+    return acumulador
